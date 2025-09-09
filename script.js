994ec5a56f08d27156c2197f213632a70fcf471e
claeswdev <claeswdev@gmail.com>
2025-09-09 08:49:23 +0200

    Add logic for generating hand signal

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+function getComputerChoice() {
+    /* Pseudo code
+    GENERATE a random number between 1-3
+    MAP the generated number to either "rock", "paper" or "scissors"
+    RETURN that value
+    */
+    let randomNumber = getRandomValue(1, 3);
+    let handSignal = getHandSignal(randomNumber);
+
+    return handSignal;
+}
+
+function getRandomValue(lowest, highest) {
+    if (typeof lowest !== "number" || typeof highest !== "number")
+        throw TypeError("Arguments must be numbers");
+
+    return Math.floor(Math.random() * highest) + lowest;
+}
+
+function getHandSignal(num) {
+    if (typeof num !== "number") throw TypeError("Arg <num> must be a number");
+
+    if (num === 1) return "rock";
+    else if (num === 2) return "paper";
+    else if (num === 3) return "scissors";
+}
+
+console.log(getComputerChoice());
